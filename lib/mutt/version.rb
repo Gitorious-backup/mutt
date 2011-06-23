2478aa13525700a19dbcd3fe6dc6288ba9bf9e9a
Christian Johansen <christian@gitorious.org>
2011-06-23 14:23:09 +0200

    Move version to separate file

--- lib/mutt/version.rb
+++ lib/mutt/version.rb
@@ -15,4 +15,7 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #++
-require "mutt/version"
+
+module Mutt
+  VERSION = "0.1.0"
+end
