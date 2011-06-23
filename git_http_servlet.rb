aa45331c19238d4cbb10205d7f0767362ae9f9f1
Marius Mathiesen <marius@gitorious.org>
2011-06-23 07:49:38 +0200

    Moving the servlet into its own file, re-use configuration

--- git_http_servlet.rb
+++ git_http_servlet.rb
@@ -15,6 +15,7 @@ require "bundler/setup"
 require 'mutt/gitorious/config'
 require 'mutt/gitorious/service'
 require 'mutt/gitorious/resolver'
+require 'mutt/gitorious/servlet'
 require 'mutt/basic_auth_handler'
 require 'mutt/gitorious/authenticator'
 require 'mutt/gitorious/receive_pack_factory'
@@ -22,43 +23,23 @@ require 'mutt/gitorious/repository_router'
 
 java_import 'org.eclipse.jgit.http.server.GitServlet'
 
-# Our servlet, based on JGit's GitServlet
-# Attaches a custom resolver, otherwise all normal
-class GitoriousServlet < GitServlet
-  def init(servlet_config)
-    path = File.join(ENV['GITORIOUS_ROOT'], 'config', 'gitorious.yml')
-    configuration = Mutt::Gitorious::Config.new(path, ENV['RAILS_ENV'] || 'production')
-    service = Mutt::Gitorious::Service.new(configuration.host, configuration.port)
-    router = Mutt::Gitorious::RepositoryRouter.new(service, configuration.repo_root)
-    resolver = Mutt::Gitorious::Resolver.new(router)
-    self.repository_resolver = resolver
-    self.receive_pack_factory = Mutt::Gitorious::ReceivePackFactory.new(service, router)
-    super
-  end
-end
 
-class StaticAuthenticator
-  def authenticate(username, password)
-    username == "marius"
-  end
-end
+path = File.join(ENV['GITORIOUS_ROOT'], 'config', 'gitorious.yml')
+
+configuration = Mutt::Gitorious::Config.new(path, ENV['RAILS_ENV'] || 'production')
 
 jetty_port = (ENV['JETTY_PORT'] || '8080').to_i
 
 # Embedding Jetty
 server = org.mortbay.jetty.Server.new(jetty_port)
 root = org.mortbay.jetty.servlet.Context.new(server, '/', org.mortbay.jetty.servlet.Context::SESSIONS)
-servlet = GitoriousServlet.new
+servlet = Mutt::Gitorious::Servlet.new(configuration)
 holder = org.mortbay.jetty.servlet.ServletHolder.new(servlet)
 
 # Attach GitoriousServlet to anything
 root.add_servlet(holder, '/*')
 
-# TODO: Remove duplication
-path = File.join(ENV['GITORIOUS_ROOT'], 'config', 'gitorious.yml')
-config = Mutt::Gitorious::Config.new(path, ENV['RAILS_ENV'] || 'production')
-
-root.security_handler = Mutt::BasicAuth::Handler.new(Mutt::Gitorious::Authenticator.new(config.db_config))
+root.security_handler = Mutt::BasicAuth::Handler.new(Mutt::Gitorious::Authenticator.new(configuration.db_config))
 
 server.start
 
---
+++ lib/mutt/gitorious/servlet.rb
@@ -0,0 +1,44 @@
+# encoding: utf-8
+#--
+#   Copyright (C) 2011 Gitorious AS
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#++
+require 'java'
+require 'org.eclipse.jgit'
+java_import 'org.eclipse.jgit.http.server.GitServlet'
+
+
+module Mutt
+  module Gitorious
+    class Servlet < GitServlet
+      attr_reader :configuration
+      
+      def initialize(configuration)
+        super()
+        @configuration = configuration
+      end
+      
+      def init(servlet_config)
+        path = File.join(ENV['GITORIOUS_ROOT'], 'config', 'gitorious.yml')
+        service = Mutt::Gitorious::Service.new(configuration.host, configuration.port)
+        router = Mutt::Gitorious::RepositoryRouter.new(service, configuration.repo_root)
+        resolver = Mutt::Gitorious::Resolver.new(router)
+        self.repository_resolver = resolver
+        self.receive_pack_factory = Mutt::Gitorious::ReceivePackFactory.new(service, router)
+        super
+      end
+    end
+  end
+end
